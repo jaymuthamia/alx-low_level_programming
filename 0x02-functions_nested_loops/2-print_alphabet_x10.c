aef6f6379a840736ff59418461783c360f5fe227
jaymuthamia <muthamiajesse@gmail.com>
2023-07-18 12:43:54 -0700

    4-isalpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabet_10 - prints the alphabet 10 times
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a'
+			while (ch <= z)
+			{
+				_putcha(ch);
+				ch++;
+			}
+		_putchar('\n');
+		i++;
+	}
+}
+

d8869c4b3ac9ea2d2ec8afed57118df068081e70
jaymuthamia <muthamiajesse@gmail.com>
2023-07-18 17:14:17 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 9-main.c 9-times_table.c -o 9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * times-table - prints the times table, starting with 0
+ * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void)
 {
@@ -12,24 +12,25 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
-		if (j == 0)
-		{
-			_putchar(k + '0');
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
-		if  (k < 10 && j != o)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(k + '0');
-		} else if (k >= 10)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-}
 }

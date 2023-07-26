b82ac347c91e3bb057e8a472ee24e7f587b61b9c
jaymuthamia <muthamiajesse@gmail.com>
2023-07-26 11:44:38 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+/**
+ * string_toupper - Change a string to uppercase
+ * @str: char str
+ *
+ * Return: char
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+
+	return (str);
+}
